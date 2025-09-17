8ba97a5d6b1257efbb578b357f1ac54d7e8aad87
2501024 <woongchul55@gmail.com>
2025-09-17 15:27:08 +0900

    3 주차

---
+++ week03/sample3-1/src/Main.java
@@ -0,0 +1,25 @@
+
+        public class Main {
+            public static void main(String[] args) {
+                int a = 0;
+                float b = 0.0f;
+                long c = 0L;
+                byte d = 0;
+                short f = 0;
+                boolean t = false;
+                char g = ' ';
+
+                final float PI = 3.141592f;
+
+
+                String y = null;
+
+                a = 5;
+                b = 5.5f;
+                c = 5;
+
+                a = (int) 6L;
+                d = (byte) 128;
+                f = (short) 32768;
+                a = a + 5;
+}}
