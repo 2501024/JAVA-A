e3e36f4a138445fb7924b05b8323555d39eebfc3
2501024 <woongchul55@gmail.com>
2025-11-26 15:31:56 +0900

    12주차

---
+++ week12/sample12-1/src/Main.java
@@ -0,0 +1,17 @@
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        int first, second;
+        int result;
+
+        Output.display();                              // call by name
+        first = Input.readData("첫번째 데이터 입력 : ");  // call by value
+        second = Input.readData("두번째 데이터 입력 : ");
+        result = Compute.add(first, second);
+        Output.display(first, second, result);
+        Output.display();
+    }
+}
