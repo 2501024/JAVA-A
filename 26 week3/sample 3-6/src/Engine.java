65dafe08a03916fa7db4ca98d647e356050da9dc
2501024 <woongchul55@gmail.com>
2026-03-18 19:15:47 +0900

    3주차 학습 자료

---
+++ 26 week3/sample 3-6/src/Engine.java
@@ -0,0 +1,12 @@
+
+public class Engine {
+
+    public void start() {
+        System.out.println("Engine Stating....");
+    }
+
+    public void stop() {
+        System.out.println("Engine Stopping....");
+    }
+
+}
