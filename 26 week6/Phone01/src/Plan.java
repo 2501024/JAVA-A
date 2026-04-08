f7b88a554296e4d91fdf44309d93140dd0ed01a1
2501024 <woongchul55@gmail.com>
2026-04-08 23:28:28 +0900

    6주차 학습 자료

---
+++ 26 week6/Phone01/src/Plan.java
@@ -0,0 +1,17 @@
+
+public abstract class Plan {
+    private String name;
+    protected final int NETWORK = 3200;
+
+    public Plan() {
+    }
+
+    public Plan(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
