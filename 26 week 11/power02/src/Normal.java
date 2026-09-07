0a2e1bb857f8f0ce2bbf2f0ed238c28ac430dcfa
2501024 <woongchul55@gmail.com>
2026-09-07 12:40:15 +0900

    Delete 26 week 11 directory

--- 26 week 11/power02/src/Normal.java
+++
@@ -1,6 +0,0 @@
-public class Normal extends Customer{
-
-    public Normal(String name, String customID, int powerUsage) {
-        super(name, customID, powerUsage);
-    }
-}
--- 26 week 11/power03/src/Normal.java
+++
@@ -1,8 +0,0 @@
-public class Normal extends Customer{
-    public Normal() {
-    }
-
-    public Normal(String name, String customID, int powerUsage) {
-        super(name, customID, powerUsage);
-    }
-}
