c812fda25d0fa4fbc009f234fff1f0525a88d16f
2501024 <woongchul55@gmail.com>
2026-09-07 12:41:01 +0900

    Delete 26 week3 directory

--- 26 week3/sample 3-5/src/Car.java
+++
@@ -1,9 +0,0 @@
-
-public class Car {
-
-    public void drive(Navigation navigation, String start, String destination) {
-        System.out.println("운전 시작");
-        navigation.findRoute(start, destination);
-        System.out.println("Driving to " + destination);
-    }
-}
